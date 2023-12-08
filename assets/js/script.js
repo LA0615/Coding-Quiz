fd0bb8dbcf2f6cf6f493cbffdb459e0ff16839ba
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2023-12-07 22:29:36 -0500

    html changes

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//When I click the START button, a timer starts
+
+//Questions are displayed one after another when a user answers
+
+//Time is subtracted when a question is answered incorrectly. 
+
+//Set interval for timer 
+
+//game is over when timer reaches 0 or all questions are answered
+
+//when the game is over, I can save my initials and my score
+
+//Event listener
